66274b7ee3ba237de9d2643c13d6f3be94121432
Siddharth Yadav <100304402+Sidlyf@users.noreply.github.com>
2023-02-28 21:46:18 +0530

    Final changes done

--- script.js
+++ script.js
@@ -71,3 +71,7 @@ $(document).ready(function () {
       },
     });
   });
+
+  const mydownload=()=>{
+    window.open("https://drive.google.com/file/d/1sNJCk3v6fLXMraHQEgCiRPv94m-JXqjz/view?usp=sharing")
+  }
